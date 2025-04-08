2cb305f5a0bb10c5284aaa4e1ae3c28ce8a14f01
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2025-04-08 12:51:50 +0530

    commit

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,9 +12,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { signUp } from "@/lib/actions/auth.action";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -59,6 +59,21 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success("Account Created Successfully. Please Sign In");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Signin failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email, idToken,
+        });
+
         toast.success("Signed In Successfully");
         router.push("/");
       }
