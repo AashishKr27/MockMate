e4dcef5be0c6c0450a4f7940848acd56bb6ba3d3
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2025-04-09 11:35:39 +0530

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,16 +12,15 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
     }
     return (
       <div className='root-layout'>
-        <div className='root-layout'>
-          <nav className=''>
-            <Link href={"/"} className='flex items-center gap-2'>
-              <Image src={"/logo.svg"} alt='logo' width={38} height={32} />
-              <h2 className='text-primary-100'>Mock<span className='text-yellow-400'>Mate</span></h2>
-            </Link>
-          </nav>
-          {children}
-        </div>
+        <nav className=''>
+          <Link href={"/"} className='flex items-center gap-2'>
+            <Image src={"/logo.svg"} alt='logo' width={38} height={32} />
+            <h2 className='text-primary-100'>Mock<span className='text-yellow-400'>Mate</span></h2>
+          </Link>
+        </nav>
+        {children}
       </div>
+
     );
   } catch (e) {
     console.error("Layout Auth Error:", e);
